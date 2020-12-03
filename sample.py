6d57cba2fe8c8a46522856b711d804e42b4a6205
KIRUBA KARUPPANNAN <61502293+kirubamay20@users.noreply.github.com>
2020-12-03 21:14:16 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from keras.datasets import mnist
+import numpy as np
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+for i in np.random.randint(0,10000+1, 10):
+  arr2im=Image.fromarray(X_trian[i])
+  arr2im.save('{}.png'.format(i), "PNG")
